56b05004e1ff17dedc4028e395db2d7f195ab700
Derek Kweon <kdh4646@hanmail.net>
2023-09-19 00:36:27 -0700

    2023-09-19 react-redux setting

---
+++ src/store/index.js
@@ -0,0 +1,23 @@
+import { createStore } from "redux";
+
+//Reducer Function
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  return state;
+};
+
+//Redux Store
+const store = createStore(counterReducer);
+
+export default store;
